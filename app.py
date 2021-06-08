91020a167e7e2a936569ea8eaa045a71721c162d
Rohan A R <rohanar@Rohans-MacBook-Pro.local>
2021-06-08 18:43:27 -0500

    sjhdvfsd

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
 	return render_template('index.html')
 
-@app.route("/takedata",methods=["POST","GET"])
+@app.route('/takedata',methods=["POST","GET"])
 def search():
-	return render_template("page.html")
+	return render_template('page.html')
