6c1efb4abae781a02acc1719a6e891b24e56d54d
Rohan A R <rohanar@Rohans-MacBook-Pro.local>
2021-06-08 18:22:50 -0500

    Good1

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
 	return render_template('index.html')
+
+@app.route("/takedata",methods=["POST","GET"])
+def search():
+	return "Hello"
