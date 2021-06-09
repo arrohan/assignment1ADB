5dc48d70f95c94e2523251604d904ae9f59ac378
Rohan A R <rohanar@Rohans-MacBook-Pro.local>
2021-06-08 22:34:16 -0500

    recentcommit

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-import re
 import csv
 app = Flask(__name__)
 
@@ -21,13 +20,5 @@ def searchdata():
 
 @app.route('/saldata',methods=["POST","GET"])
 def saldata():
-	people=[]
 	sal = request.form.get("salBar")
-	sal = float(sal)
-	for items in data:
-		salary = 0
-		if(items[2] != ''):
-			salary = float(items[2])
-		if salary > sal:
-			people.append(items)
-	return render_template('salbaseddata.html',dict=people, sal=sal)
+	return render_template('salbaseddata.html',dict=data, sal=sal)
