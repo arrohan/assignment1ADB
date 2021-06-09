b26669ff9558617c58f819832cb5bd97a9c56c32
Rohan A R <rohanar@Rohans-MacBook-Pro.local>
2021-06-08 19:46:46 -0500

    recentcommit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
 	return render_template('index.html')
 
-@app.route('/takedata',methods=["POST","GET"])
+@app.route('/alldata',methods=["POST","GET"])
 def search():
 	data = list(csv.reader(open('people.csv')))
 	return render_template('page.html',dict=data)
